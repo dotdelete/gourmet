462c7f0657ab61c61770993bd0a5fa9e8683873a
Élie Caratgé <elie@bsport.io>
2025-03-25 10:46:23 +0100

    feat: add image domains to next.config for improved image handling

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'standalone',
+    images: {
+        domains: [
+            'media.istockphoto.com', 
+            'www.croquonslavie.fr', 
+            'cache.marieclaire.fr', 
+            'www.valderance.com', 
+            'cuisine-addict.com', 
+            'img.cuisineaz.com', 
+            'chefcuisto.com', 
+            'static.750g.com'
+        ],
+    },
 };
 export default nextConfig;
